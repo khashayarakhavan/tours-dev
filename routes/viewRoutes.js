7334fc6a7146d3c4eb8271e94c8b5b6459a97312
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-06-15 07:38:42 -0800

    Finish user account page

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,9 +5,8 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.get('/', authController.isLoggedIn, viewsController.getOverview);
-
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
-router.get('/login', viewsController.getLoginForm);
-// router.get('/logout', viewsController.getLoginForm);
+router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
+router.get('/me', authController.protect, viewsController.getAccount);
 
 module.exports = router;
