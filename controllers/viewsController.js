17405a7df2c31c3a3907fbcf5c116681237048b3
AftoflBIG5 <AftoflBIG5@gmail.com>
2020-06-19 10:12:32 -0800

    Add default profile photo

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -69,6 +69,7 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   // 2) Find tours with the returned IDs
   const tourIDs = bookings.map(el => el.tour);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
+  console.log('Hello from getMyTours !');
 
   res.status(200).render('overview', {
     title: 'My Tours',
